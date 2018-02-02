f812c9f32d1767c0afddaa35b5c2b67315e395f9
Andrew Nisbet <anisbet@epl.ca>
2018-02-01 18:00:03 -0700

    Needs work, don't distribute.

--- pipe.pl
+++ pipe.pl
@@ -27,7 +27,7 @@
 # Created: Mon May 25 15:12:15 MDT 2015
 #
 # Rev:
-# 0.46.00 - Jan 31, 2018 Add -g function to -X and -Y.
+# 0.46.05 - Feb 1, 2018 Add 'any' keyword to -o function.
 #
 ####################################################################################
 
@@ -37,7 +37,7 @@ use vars qw/ %opt /;
 use Getopt::Std;
 
 ### Globals
-my $VERSION           = qq{0.46.00};
+my $VERSION           = qq{0.46.05};
 my $KEYWORD_ANY       = qw{any};
 # Flag means that the entire file must be read for an operation like sort to work.
 my $LINE_RANGES       = {};
@@ -126,7 +126,7 @@ sub usage()
        -W{delimiter}
        -14bBcovwzZ{c0,c1,...,cn}
        -3{c0:n,c1:m,...,cn:p}
-       -nOtu{[any|c0,c1,...,cn]}
+       -noOtu{[any|c0,c1,...,cn]}
        -C{[any|cn]:(gt|lt|eq|ge|le)exp,...}
        -ds[-IRN]{c0,c1,...,cn} [-J[cn]]
        -e[c0:[uc|lc|mc|us],...}
@@ -308,7 +308,8 @@ All column references are 0 based. Line numbers start at 1.
                   switch to preserve keys' case during comparison. See -n, and -I.
                   Outputs absolute value of -a, -v, -1, -3, -4, results.
                   Causes summaries to be output with delimiter to STDERR on last line.
- -o{c0,c1,...cn}: Order the columns in a different order. Only the specified columns are output.
+ -o{any|c0,c1,...cn}: Order the columns in a different order. Only the specified columns are output, unless.
+                  the keyword 'any' is used, then the remaining columns are output in left-to-right order.
  -O{any|c0,c1,...cn}: Merge columns. The first column is the anchor column, any others are appended to it
                   ie: 'aaa|bbb|ccc' -Oc2,c0,c1 => 'aaa|bbb|cccaaabbb'. Use -o to remove extraneous columns.
                   Using the 'any' keyword causes all columns to be merged in the data in column 0.
@@ -859,7 +860,30 @@ sub order_line( $ )
 {
 	my $line    = shift;
 	my @newLine = ();
-	foreach my $colIndex ( @ORDER_COLUMNS )
+	my @order_columns = ();
+	foreach my $c ( @ORDER_COLUMNS )
+	{
+		# If the keyword any is used push all the missing columns of the line onto @order_columns.
+		# Other lines might have different numbers of columns.
+		# now add all the columns that aren't on the array already.
+		last if ( $c =~ m/($KEYWORD_ANY)/i );
+		unshift @order_columns, $c;
+	}
+	if ( grep /($KEYWORD_ANY)/i, @ORDER_COLUMNS )
+	{
+		foreach my $colIndex ( 0 .. scalar( @{ $line } ) -1 )
+		{
+			next if ( grep /($colIndex)/, @order_columns );
+			unshift @order_columns, $colIndex;
+		}
+	}
+	printf STDERR "order of columns: ";
+	foreach my $c ( @order_columns )
+	{
+		printf STDERR "%d, ", $c;
+	}
+	printf STDERR "\n";
+	foreach my $colIndex ( @order_columns )
 	{
 		if ( defined @{ $line }[ $colIndex ] )
 		{
@@ -2957,7 +2981,8 @@ sub init
 	@NO_COMPARE_COLUMNS= read_requested_columns( $opt{'B'}, 0 )                             if ( $opt{'B'} );
 	@NORMAL_COLUMNS    = read_requested_columns( $opt{'n'}, $KEYWORD_ANY )                  if ( $opt{'n'} );
 	@MERGE_COLUMNS     = read_requested_columns( $opt{'O'}, $KEYWORD_ANY )                  if ( $opt{'O'} );
-	@ORDER_COLUMNS     = read_requested_columns( $opt{'o'}, 0 )                             if ( $opt{'o'} );
+	@ORDER_COLUMNS     = read_requested_columns( $opt{'o'}, $KEYWORD_ANY )                  if ( $opt{'o'} );
+	# @ORDER_COLUMNS     = read_requested_columns( $opt{'o'}, 0 )                             if ( $opt{'o'} );
 	@TRIM_COLUMNS      = read_requested_columns( $opt{'t'}, $KEYWORD_ANY )                  if ( $opt{'t'} );
 	if ( $opt{'2'} )
 	{
